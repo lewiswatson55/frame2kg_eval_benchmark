d2ccff23097b9789baa7f52006bb3421f4a784ae
lewiswatson55 <lewiswatson55@protonmail.com>
2025-10-08 16:49:17 +0100

    add manifest and JSON files for video data representation

--- setup.py
+++ setup.py
@@ -18,7 +18,6 @@ setup(
     classifiers=[
         "Development Status :: 4 - Beta",
         "Intended Audience :: Science/Research",
-        "License :: OSI Approved :: MIT License",
         "Programming Language :: Python :: 3",
         "Programming Language :: Python :: 3.8",
         "Programming Language :: Python :: 3.9",
