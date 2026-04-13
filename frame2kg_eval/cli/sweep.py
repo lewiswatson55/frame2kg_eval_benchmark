7a4806149a4c58fc58b7283b2d7f0b113bf6f74e
lewiswatson55 <lewiswatson55@protonmail.com>
2026-04-13 03:37:24 +0100

    Implement cache clearing for text similarity computers to optimise memory usage

--- frame2kg_eval/cli/sweep.py
+++ frame2kg_eval/cli/sweep.py
@@ -11,7 +11,7 @@ from frame2kg_eval.cli.evaluate import load_config
 from frame2kg_eval.io.preds import PredictionLoader
 from frame2kg_eval.io.groundtruth import create_ground_truth_adapter
 from frame2kg_eval.matching.assign import two_stage_node_match
-from frame2kg_eval.matching.text import TextSimilarityComputer
+from frame2kg_eval.matching.text import TextSimilarityComputer, clear_text_computer_caches
 from frame2kg_eval.metrics.nodes import node_prf1, aggregate_micro
 from frame2kg_eval.metrics.edges import edge_prf1
 from frame2kg_eval.utils.logging import logger
@@ -184,6 +184,8 @@ def main(pred_dir, gt, taus, alphas, text_mode, text_floor, out, config, verbose
                 text_computer=semantic_text_computer,
             )
             edge_metrics_list.append(edge_metrics)
+
+            clear_text_computer_caches(shared_text_computer, semantic_text_computer)
         
         # Aggregate metrics
         node_micro = aggregate_micro(node_metrics_list)
